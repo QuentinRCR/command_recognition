d3392acc663064cef13fd591eac852d250416050
QuentinRCR <reyquentin.rq@gmail.com>
2023-10-02 18:41:52 +0200

    working in french

--- language_processing.py
+++ language_processing.py
@@ -1,50 +1,48 @@
 import speech_recognition as sr
-import nltk
-from nltk.stem.porter import PorterStemmer
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet
-from nltk.stem.snowball import SnowballStemmer
+import spacy
 
+# elements to detect
+list_commands = set(["avancer","stopper","gauche","droite","faire","transporter"])
+list_actions = ['prendre','déposer']
+list_products = ['bâton','planche']
+
+
+follow_up_command = {'faire': list_actions, 'transporter':list_products}
+
+# language processing
+nlp = spacy.load('fr_core_news_md')
+
+# Speech to text
 recognizer = sr.Recognizer()
-
 with sr.Microphone() as source:
     print("Listening")
     audio = recognizer.listen(source)
 
     try:
-        recognized_text = recognizer.recognize_google(audio)
+        recognized_text = recognizer.recognize_google(audio, language="fr-FR")
     except sr.UnknownValueError:
         print("Could not understand audio")
 
-list_commands = set(["forward","stop","left","right","do","transport"])
-list_actions = ['take','drop']
-list_products = ['stick','planck']
 
-next_command = {'do': list_actions, 'tranport':list_products}
-
-# recognized_text = 'go forth'
-
-stemmer = PorterStemmer()
-lemmatizer = WordNetLemmatizer()
-
-tokens = nltk.tokenize.word_tokenize(recognized_text)
+#tokenise
+tokens = nlp(recognized_text) 
 print(tokens)
 
-radicals = [stemmer.stem(token) for token in tokens]
-print(radicals)
+# transform to radicals
+radical = [token.lemma_ for token in tokens] 
+print(radical)
 
-radic = [lemmatizer.lemmatize(token, wordnet.VERB) for token in radicals]
 
-print(radic)
-radic = set(radic)
+radical = set(radical)
+detected_commands = radical.intersection(list_commands) #get intersection 
 
-detected_commands = radic.intersection(list_commands)
-if len(detected_commands)>0:
+if len(detected_commands)>0: # if there is a matching command
     detected_command = detected_commands.pop()
     print(f'Command detected: {detected_command}')
 
-    if detected_command in next_command:
-        detected_follow_ups = radic.intersection(next_command[detected_command])
+
+    if detected_command in follow_up_command: # check if this command have a follow up command
+        detected_follow_ups = radical.intersection(follow_up_command[detected_command])
         if len(detected_follow_ups)>0:
             detected_follow_up = detected_follow_ups.pop()
             print(f'Follow up command is: {detected_follow_up}')
